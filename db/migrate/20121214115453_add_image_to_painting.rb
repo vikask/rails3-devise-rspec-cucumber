1bee810415bd58b434dad22cb2400df55bdb2aee
vikas <vikask@swiftsetup.com>
2012-12-17 10:56:06 +0530

    use carrierwave to upload pics

---
+++ db/migrate/20121214115453_add_image_to_painting.rb
@@ -0,0 +1,9 @@
+class AddImageToPainting < ActiveRecord::Migration
+  def self.up
+    add_column :paintings, :image, :string
+  end
+
+  def self.down
+    remove_column :paintings, :image
+  end
+end
